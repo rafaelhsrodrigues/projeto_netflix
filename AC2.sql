dcb74a5d70dd49c773d640f5a1803e48fec673ab
rafaelhsrodrigues <105739564+rafaelhsrodrigues@users.noreply.github.com>
2023-10-07 14:42:10 -0300

    Add files via upload

---
+++ AC2.sql
@@ -0,0 +1,34 @@
+/*
+PROJETO NETFLIX: Atraves de dados, analisar quais tipos de series fazem sucesso no site de Reviews
+e encontrar algum padrão para a Netflix investir em uma proxima produção
+
+Para esta AC:
+		1 - visualizar apenas as series
+		2 - visualizar as series bem avaliadas (70+)
+*/
+
+
+
+SELECT
+	*
+FROM netflix
+
+--visualizar apenas as series e as colunas que serão analisadas
+SELECT
+	title, type, release_year, age_certification, runtime, genres, production_countries, seasons, imdb_score, imdb_votes
+FROM 
+	netflix
+WHERE
+	type = 'SHOW'
+
+
+--visualizar as series bem avaliadas (70+)
+SELECT
+	title, type, release_year, age_certification, runtime, genres, production_countries, seasons, imdb_score, imdb_votes
+FROM 
+	netflix
+WHERE
+	type = 'SHOW'
+	AND
+	imdb_score >= 70
+
